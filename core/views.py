9261737ae2b2f4c5378db7c1b9529100cc461c0f
Gerardo <peralta.gerardo97@gmail.com>
2019-06-18 08:57:34 +0200

    Primer commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def registrarcliente(request):
+    return render(request,"core/registrarcliente.html")
+
+def buscarcliente(request):
+    return render(request,"core/buscarcliente.html")
+
+def menucliente(request):
+    return render(request,"core/menucliente.html")
+
+def devoluciondevehiculo(request):
+    return render(request,"core/devoluciondevehiculo.html")
+
+
+
+        
