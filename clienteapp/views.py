77173bf5ddc4aaf352e89e5734cfc49f6e27b7a1
Gerardo <peralta.gerardo97@gmail.com>
2019-06-20 19:15:29 +0200

    Se muestran los clientes en la tabla buscar cliente

--- clienteapp/views.py
+++ clienteapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import cliente
 
 # Create your views here.
 def registrarcliente(request):
     return render(request,"clienteapp/registrarcliente.html")
+
+def buscarcliente(request):
+    list_cliente = cliente.objects.all()
+    return render(request,"clienteapp/buscarcliente.html",{'list_cliente':list_cliente})
--- core/views.py
+++ core/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def registrarcliente(request):
-    return render(request,"core/registrarcliente.html")
-
-def buscarcliente(request):
-    return render(request,"core/buscarcliente.html")
-
 def menucliente(request):
     return render(request,"core/menucliente.html")
 
