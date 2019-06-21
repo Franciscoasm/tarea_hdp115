75ca48cd7b68fe750a96d531dd63786b5a263134
Daniel <danielenrique9919@hotmail.com>
2019-06-21 09:55:05 -0600

    Se modifico el ancho de la flecha del clienteapp

--- apps/clienteapp/models.py
+++ apps/clienteapp/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class cliente(models.Model):
-    ididentificador=models.CharField(max_length=100, null=True, blank=True, unique=True)
-    nombrecliente=models.CharField(max_length=30)
-    apellidocliente=models.CharField(max_length=30)
-    fechanacimiento=models.DateTimeField()
-    direccion=models.TextField()
-    telefono=models.CharField(max_length=15) 
-    identificador=models.CharField(max_length=200) 
+    ididentificador = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    nombrecliente = models.CharField(max_length=30)
+    apellidocliente = models.CharField(max_length=30)
+    fechanacimiento = models.DateTimeField()
+    direccion = models.TextField()
+    telefono = models.CharField(max_length=15) 
+    identificador = models.CharField(max_length=200) 
 
     class Meta:
         ordering=["-ididentificador"]
